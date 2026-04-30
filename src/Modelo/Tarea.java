17a3be3cb0347298c5ed32a863983e48317a5b88
usuri <usuri@ZUKER>
2026-04-30 11:01:56 -0500

    Entrega 1 - estructura inicial MVC

---
+++ src/Modelo/Tarea.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author usuri
+ */
+public class Tarea {
+   
+    private String codigo;
+    private String titulo;
+}
